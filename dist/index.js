507dc310d43a36dc2f96a24ba42c543bccbb56d1
Jon Washington <jonathan.washington@gmail.com>
2023-03-21 17:17:24 -0700

    Delivered hello world message on index page

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,11 @@
 "use strict";
-//import * as express from 'express'
-//const app = express()
-//app.listen(3000, ()=> console.log("listening..."))
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const app = express();
+app.listen(3000, () => console.log("listening..."));
+app.use(express.static('./src'));
+app.get('/', function (request, response) {
+    response.sendFile('index.html', { root: './src' });
+});
 console.log("I live!");
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-//import * as express from 'express'
-//const app = express()
-//app.listen(3000, ()=> console.log("listening..."))
+import * as express from 'express'
+const app: express.Application = express()
+app.listen(3000, ()=> console.log("listening..."))
+
+app.use(express.static('./src'));
+
+app.get('/', function(request, response){
+  response.sendFile('index.html', {root: './src'});
+});
+
 
 console.log("I live!")
