adb87535b1bf3089f967fb53f1b4b93cc35f95ee
Jon Washington <jonathan.washington@gmail.com>
2023-03-28 15:01:39 -0700

    Add header, align items, and adjust line styling

--- src/components/Body.js
+++ src/components/Body.js
@@ -9,7 +9,9 @@ class Body extends React.Component{
 
     render(){
         return (
-            <div className='flex-child-element'>
+            <div className='flex-child-element main-content'>
+                <h1>Jon Washington</h1>
+                <h2>Software Engineer</h2>
                 <a>{bodyText[0].text}</a>
             </div>
         )
