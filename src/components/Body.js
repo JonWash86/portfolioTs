94576e570869675849dfdef8a4446ce2cfdf672e
Jon Washington <jonathan.washington@gmail.com>
2023-03-30 16:10:34 -0700

    Implement favicon, hover effects, and headling/subheading styling

--- src/components/Body.js
+++ src/components/Body.js
@@ -10,8 +10,8 @@ class Body extends React.Component{
     render(){
         return (
             <div className='flex-child-element main-content'>
-                <h1>Jon Washington</h1>
-                <h2>Software Engineer</h2>
+                <h1 id="headline">Jon Washington</h1>
+                <h2 id="subheading">Software Engineer</h2>
                 <a>{bodyText[0].text}</a>
             </div>
         )
