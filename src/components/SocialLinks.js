2a8ada5eb639e32e36993976352f65d7dda87603
Jon Washington <jonathan.washington@gmail.com>
2023-04-06 14:16:49 -0700

    Move social icons and build them in SocialLinks component

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
+import socials from '../data/socials.json';
+
 class SocialLinks extends React.Component {
     constructor(props){
         super(props);
     }
 
     render(){
+        let links = socials.map(item => 
+            <a className='social-icon' href={item.href} target='blank'><img src={item.icon} width='45px' height='45px' alt={item.name} /></a>
+        )
+
         return(
-            <div className='social-links'>Social Links will sit here!</div>
+            // <div className='social-links'>Social Links will sit here!</div>
+            <div className='social-links'>{links}</div>
         )
     }
 }
